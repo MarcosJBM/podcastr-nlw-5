6fb3b61bd33126e3122ad15117722273690eacd8
Marcos Botene <marcosjbotene@gmail.com>
2021-04-23 21:33:01 -0300

    Created: formatting the received data.

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,7 +2,16 @@ type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: File;
 };
+
+interface File {
+  url: string;
+  duration: number;
+}
 
 type HomeProps = {
   episodes: Episode[];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import { api } from '../services/api';
 
@@ -11,7 +13,7 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('episodes', {
+  const data: Episode[] = await api.get('episodes', {
     params: {
       _limit: 12,
       _sort: 'published_at',
@@ -19,9 +21,24 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     },
     revalidate: 60 * 60 * 8,
   };
