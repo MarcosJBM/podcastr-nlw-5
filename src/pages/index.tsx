d852904a8fdbd14dabe405f551543c7623af4224
Marcos Botene <marcosjbotene@gmail.com>
2021-04-23 21:10:20 -0300

    Created: services folder created.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import { api } from '../services/api';
+
 export default function Home(props: HomeProps) {
   return (
     <div>
@@ -9,10 +11,13 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch(
-    'http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=des'
-  );
-  const data = await response.json();
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
 
   return {
     props: {
