2318cb8f02a46788879fb809dffe7bdeec3c6add
Marcos Botene <marcosjbotene@gmail.com>
2021-04-24 00:24:40 -0300

    Created: structuration for the Episode page.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,7 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
-import { useRouter } from 'next/router';
-
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
@@ -9,12 +7,49 @@ import { api } from '../../services/api';
 
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
+import styles from './episode.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
 interface Episodes {
   episode: Episode;
 }
 
 export default function Episode({ episode }: Episodes) {
-  return <h1>{episode.title}</h1>;
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href='/'>
+          <button type='button'>
+            <img src='/arrow-left.svg' alt='Voltar' />
+          </button>
+        </Link>
+
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit={'cover'}
+        />
+
+        <button type='button'>
+          <img src='/play.svg' alt='Tocar episódio' />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.published_at}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
